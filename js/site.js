d56a581564fc491e160155557ba46608e0813a1a
FatimahSyed27 <89406364+FatimahSyed27@users.noreply.github.com>
2023-04-02 15:12:29 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,9 +1,3 @@
-// jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
-});
-
 // Function to display message after submit
 function sayCode() {
    alert("Thank you for filling out the form! Please check your email for any job updates!");
